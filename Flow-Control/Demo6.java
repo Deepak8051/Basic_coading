9b4f7238d4e85dc6a1814a1e12879020f607cd1d
Deepak kumar Sharma <101473013+Deepak8051@users.noreply.github.com>
2023-09-24 20:04:06 +0000

    this keyword

---
+++ Flow-Control/Demo6.java
@@ -0,0 +1,17 @@
+
+//Write a program to Print 1 to N numbers?
+import java.util.Scanner;
+public class Demo6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n  = sc.nextInt();
+        for (int i=1; i<=n;i++){
+            System.out.println(i);
+
+        }
+    }
+    
+
+    
+}
---
+++ Flow-Control/Demo7.java
@@ -0,0 +1,14 @@
+//Write a program to Print REVERSE of N to 1 numbers?
+import java.util.Scanner;
+public class Demo7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int n = sc.nextInt();
+        for(int i=n; i>=1; i--)
+        System.out.println(i);
+        
+    }
+
+    
+}
