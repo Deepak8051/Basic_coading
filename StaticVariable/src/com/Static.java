f215f25eca72f1e36788eedfa99ddb4dbe1b6eb0
Deepak kumar Sharma <101473013+Deepak8051@users.noreply.github.com>
2023-11-18 23:54:33 +0530

    Add files via upload

---
+++ StaticVariable/src/com/Static.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Static {
+	static int a= 100;
+	static int b=34;
+	public static void main(String[]args) {
+		System.out.println(Static.a);
+		System.out.println(Static.b);
+		 
+	}
+	
+
+}
